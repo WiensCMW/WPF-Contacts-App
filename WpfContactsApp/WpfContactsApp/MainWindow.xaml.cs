3c08ef10b5f420744991c18a92d977079a6f1e20
cornie <cornie.wiens@vidir.com>
2019-05-16 17:04:11 -0500

    Testing with SQLite insert/read performance

--- WpfContactsApp/WpfContactsApp/MainWindow.xaml.cs
+++ WpfContactsApp/WpfContactsApp/MainWindow.xaml.cs
@@ -31,10 +31,38 @@ namespace WpfContactsApp
 
         private void buttonNewContact_Click(object sender, RoutedEventArgs e)
         {
-            NewContactWindow newContactWindow = new NewContactWindow() { Owner = this };
-            newContactWindow.ShowDialog();
+            //NewContactWindow newContactWindow = new NewContactWindow() { Owner = this };
+            //newContactWindow.ShowDialog();
 
-            ReadDatabase();
+            try
+            {
+                //ReadDatabase();
+                for (int i = 0; i < 1000000; i++)
+                {
+                    using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath))
+                    {
+                        conn.CreateTable<Contact>();
+
+                        Contact newContact = new Contact()
+                        {
+                            Name = $"Billy the {i + 1}",
+                            Email = $"billy{i + 1}@gmail.com",
+                            Phone = i.ToString()
+                        };
+                        conn.Insert(newContact);
+                    }
+
+                    using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath))
+                    {
+                        conn.CreateTable<Contact>();
+                        List<Contact> contacts = conn.Table<Contact>().ToList();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
 
         private void ReadDatabase()
