12359d5e5011a89158d431b216aab34f61089566
cornie <cornie.wiens@vidir.com>
2019-05-17 09:08:10 -0500

    Reverted test changes from previous commit
    In previous commit I added some test code to test the read and write performance of a SQLite db, especially with multiple sessions hitting the same db simultaneously.

--- WpfContactsApp/WpfContactsApp/MainWindow.xaml.cs
+++ WpfContactsApp/WpfContactsApp/MainWindow.xaml.cs
@@ -31,38 +31,10 @@ namespace WpfContactsApp
 
         private void buttonNewContact_Click(object sender, RoutedEventArgs e)
         {
-            //NewContactWindow newContactWindow = new NewContactWindow() { Owner = this };
-            //newContactWindow.ShowDialog();
+            NewContactWindow newContactWindow = new NewContactWindow() { Owner = this };
+            newContactWindow.ShowDialog();
 
-            try
-            {
-                //ReadDatabase();
-                for (int i = 0; i < 1000000; i++)
-                {
-                    using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath))
-                    {
-                        conn.CreateTable<Contact>();
-
-                        Contact newContact = new Contact()
-                        {
-                            Name = $"Billy the {i + 1}",
-                            Email = $"billy{i + 1}@gmail.com",
-                            Phone = i.ToString()
-                        };
-                        conn.Insert(newContact);
-                    }
-
-                    using (SQLiteConnection conn = new SQLiteConnection(App.DatabasePath))
-                    {
-                        conn.CreateTable<Contact>();
-                        List<Contact> contacts = conn.Table<Contact>().ToList();
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
+            ReadDatabase();
         }
 
         private void ReadDatabase()
